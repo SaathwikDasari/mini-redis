fd2da1e50f37d277f9e0d0c418ef35b41b8c89f9
Saathwik Dasari <saathwik.dasari@gmail.com>
2025-11-03 02:03:07 +0530

    added basic Tcp Listener

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-mod server;
+use tokio::net::{TcpListener, TcpStream}; // ⬅️ Improved 'use' statement
+use anyhow;
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr = "127.0.0.1:6379";
+
+    let listener: TcpListener = TcpListener::bind(addr).await?;
+    println!("Server Listening on: {}", addr);
+
+    loop {
+        let (socket, client_addr) = listener.accept().await?;
+        println!("Connection established from: {}", client_addr);
+        
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(socket).await {
+                eprintln!("Error handling connection: {}", e);
+            }
+        })
+    }
+}
+
+
+async fn handle_connection(mut socket: TcpStream) -> anyhow::Result<()> {
+
+    println!("Connection Closed!");
+    Ok(())
 }
